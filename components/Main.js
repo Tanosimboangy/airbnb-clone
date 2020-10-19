4238cc301c7a21ad4b74fd07061222e5a69fd4ce
Tanosimboangy <jacquit.aja@onja.org>
2020-10-19 16:03:11 +0300

    creating all of the necessary files

---
+++ components/Main.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Main() {
+    return (
+        <div>
+            <p>I do not know how it feels but I just want it more!</p>
+        </div>
+    ) 
+}
+
---
+++ script.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    )
+}
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
