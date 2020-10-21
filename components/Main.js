6bd9d33cba90cdd0b81a02993b0e6e913f35b45e
Tanosimboangy <jacquit.aja@onja.org>
2020-10-21 16:28:47 +0300

    showing the popup

--- components/Main.js
+++ components/Main.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import Element from "./Element.js";
 import stays from "./stays.json";
+import Header from "./Header.js";
+import Popup from "./Popup";
+
 
 function Main() {
     let source = stays;
     for(let i = 0;source.length > i; i++){
         source[i].id = i;
    }
-    return (
-        source.map(item => {
-            return (
-                <section key={item.id}>
-                    <Element item={item} />  
-                </section>
-            )
-        })
-    )
-}
 
+   const [isOpen, setIsOpen] = useState(false);
+    const togglePopup = () => {
+        setIsOpen(!isOpen);
+    }
+
+   const [filter, useFilter] = useState(source);
+   function HandleFiltering(e) {
+        useFilter(source.filter(result => result.city.toLowerCase() === e.target.value.toLowerCase()));
+    // console.log(filter);
+   }
+
+    return (
+        <div>
+            <div>
+                <Header togglePopup={togglePopup}/>
+                {isOpen && <Popup HandleFiltering={HandleFiltering} togglePopup={togglePopup} handleClose={togglePopup}/>}
+            </div>
+            <main>
+                {filter.map(item => {
+                    return (
+                        <section key={item.id}>
+                            <Element item={item}/>  
+                        </section>
+                    )})}
+            </main>
+        </div>
+    )}
 export default Main;
--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 function App() {
     return (
         <div>
-            <Header />
-            <main className="main">
-                <ul>
-                    <li><h2>Stays in Finland</h2></li>
-                    <li><p>12 + stays</p></li>
-                </ul>
-                <div className="wrapper_main">
-                    <Main className="main" />
-                </div>
-            </main>
+            <Main className="main"/>
             <Footer />
         </div>
     )
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+{/* <ul>
+<li><h2>Stays in Finland</h2></li>
+<li><p>12 + stays</p></li>
+</ul> */}
