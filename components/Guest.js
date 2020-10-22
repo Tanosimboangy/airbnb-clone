fd133f248ddcfe26f955265efb7182bcb09862c0
Tanosimboangy <jacquit.aja@onja.org>
2020-10-22 07:24:35 +0300

    adding Guest component

--- components/Guest.js
+++ components/Guest.js
@@ -1,22 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Guest(props) {
-  return (
-      <div className="Add_guest">
-          <div>
-            <h2>
-                Adults
-            </h2>
-            <p>Ages 13 or above</p>
-          </div>
-          <div>
-            <h2>
-                Children
-            </h2>
-            <p>Ages 2-12</p>
-          </div>
-      </div>
-  );
-};
+    const [countAdd, setCountAdd] = useState(0);
+    const [countChild, setCountChild] = useState(0);
+
+    return (
+        <div className="Add_guest">
+            <div>
+                <h2>
+                    Adults
+                </h2>
+                <p>Ages 13 or above</p>
+                <div>
+                    {countAdd}
+                    <button onClick={() => setCountAdd((currentCountAdd) => currentCountAdd - 1)}>
+                        -
+                    </button>
+                    <button onClick={() => setCountAdd((currentCountAdd) => currentCountAdd + 1)}>
+                        +
+                    </button>
+                </div>
+            </div>
+            <div>
+                <h2>
+                    Children
+                </h2>
+                <p>Ages 2-12</p>
+                <div>
+                    {countChild}
+                    <button onClick={() => setCountChild((currentCountChild) => currentCountChild - 1)}>
+                        -
+                    </button>
+                    <button onClick={() => setCountChild((currentCountChild) => currentCountChild + 1)}>
+                        +
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+    };
 
 export default Guest;
--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -28597,14 +28597,26 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function Guest(props) {
+  const [countAdd, setCountAdd] = (0, _react.useState)(0);
+  const [countChild, setCountChild] = (0, _react.useState)(0);
   return /*#__PURE__*/_react.default.createElement("div", {
     className: "Add_guest"
-  }, /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("h2", null, "Adults"), /*#__PURE__*/_react.default.createElement("p", null, "Ages 13 or above")), /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("h2", null, "Children"), /*#__PURE__*/_react.default.createElement("p", null, "Ages 2-12")));
+  }, /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("h2", null, "Adults"), /*#__PURE__*/_react.default.createElement("p", null, "Ages 13 or above"), /*#__PURE__*/_react.default.createElement("div", null, countAdd, /*#__PURE__*/_react.default.createElement("button", {
+    onClick: () => setCountAdd(currentCountAdd => currentCountAdd - 1)
+  }, "-"), /*#__PURE__*/_react.default.createElement("button", {
+    onClick: () => setCountAdd(currentCountAdd => currentCountAdd + 1)
+  }, "+"))), /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("h2", null, "Children"), /*#__PURE__*/_react.default.createElement("p", null, "Ages 2-12"), /*#__PURE__*/_react.default.createElement("div", null, countChild, /*#__PURE__*/_react.default.createElement("button", {
+    onClick: () => setCountChild(currentCountChild => currentCountChild - 1)
+  }, "-"), /*#__PURE__*/_react.default.createElement("button", {
+    onClick: () => setCountChild(currentCountChild => currentCountChild + 1)
+  }, "+"))));
 }
 
 ;
